3bf0b9c8c5e502af2daff0c853b22de248332bc9
Joey Stanley <joey_stanley@byu.edu>
2020-08-05 22:11:13 -0600

    updated example code

--- R/grapes.R
+++ R/grapes.R
@@ -1,21 +1,22 @@
 #' The "Not In" function
 #'
-#' This returns \code{TRUE} if values are \emph{not} in a list. It's the opposite of \code{\%in\%}.
-#' This is particularly good when subsetting and you only want to remove a
-#' few things (like diphthongs). You can remove those rather than specifying
-#' all monophthongs you want to keep.
+#' This returns \code{TRUE} if values are \emph{not} in a list. It's the opposite 
+#' of \code{\%in\%}. This is particularly good when subsetting and you only want
+#' to remove a few things (like diphthongs). You can remove those rather than 
+#' specifying all monophthongs you want to keep.
 #'
 #' Note that what this function does can easily be accomplished with \code{!x \%in\% y},
 #' but I like the "not in" aspect. Probably a holdover from my Perl days...
 #'
-#' You can also define this function as \code{`\%ni\%` <- Negate(`\%in\%`)} but when looking
-#' through the documentation on \%in\%, I found that it might be better to
-#' modify the formal definition there instead.
+#' You can also define this function as \code{`\%ni\%` <- Negate(`\%in\%`)} but 
+#' when looking through the documentation on \%in\%, I found that it might be 
+#' better to modify the formal definition there instead.
 #'
 #' Credit to \href{https://stackoverflow.com/questions/24660864/declaring-special-infix-functions-in-r-packages}{this Stack Overflow question} for showing how to get this to work in an R package.
 #' @param x A vector, presumably a subset of "y"
 #' @param y A vector
-#' @return A list of logicals indicating whether the objects in `x` are not contained in `y`.`
+#' @return A list of logicals indicating whether the objects in \code{x} are not
+#' contained in \code{y}.
 #' @examples
 #' 1:10 %in% c(1,3,5,9)
 #' 1:10 %ni% c(1,3,5,9)
@@ -38,7 +39,9 @@
 #' @param range A numeric vector. The highest and lowest values will be used as the range.
 #' @return A logical vector.
 #' @examples
-#' c(1:5) %wi% c(2,3)
+#' 4 %wi% c(1,5)
+#' c(2, 3) %wi% c(1,5)
+#' c(1:5) %wi% c(4,6)
 `%wi%` <- function(x, range) {
   
   return_values <- rep(FALSE, length(x))
